ebce149d621d2f505752e3348dde1085860fa108
Joaquim Perez <jivkim@gmail.com>
2021-08-05 00:30:23 +0200

    4 inputs

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,34 @@
 <template>
   <div id="app">
     <h1>Box sizer</h1>
+    <div>
+      <label for="x">x: </label>
+      <input type="number" name="x" v-model="x">
+    </div>
+    <div>
+      <label for="y">y: </label>
+      <input type="number" name="y" v-model="y">
+    </div>
+    <div>
+      <label for="z">z: </label>
+      <input type="number" name="z" v-model="z">
+    </div>
+    <div>
+      <label for="e">e: </label>
+      <input type="number" name="e" v-model="e">
+    </div>
   </div>
 </template>
 
 <script>
+export default {
+  data: () => ({
+    x: 0,
+    y: 0,
+    z: 0,
+    e: 0
+  })
+}
 </script>
 
 <style lang="scss">
@@ -14,5 +38,9 @@
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   margin: 30px;
+
+  label {
+    font-family: monospace;
+  }
 }
 </style>
