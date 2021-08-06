de2f79841ef030941f40b8762fbc083c4c279bce
Joaquim Perez <jivkim@gmail.com>
2021-08-06 15:31:01 +0200

    complete config

--- src/config.js
+++ src/config.js
@@ -4,21 +4,89 @@ export default {
       name: 'A',
       rectangles: [
         {
-          w: ({y}) => y / 2,
-          h: ({z,e}) => +z - 4 * e
+          w: ({y}) => y,
+          h: ({z}) => z
         },
         {
           w: ({x,e}) => +x + 8 * e,
           h: ({z}) => z
         },
         {
-          w: ({y}) => y / 2,
-          h: ({z,e}) => +z - 4 * e
+          w: ({y}) => y,
+          h: ({z}) => z
+        }
+      ]
+    },
+    {
+      name: 'B',
+      rectangles: [
+        {
+          w: ({z,e}) => +z + +e,
+          h: ({y,e}) => +y + 2*e
+        },
+        {
+          w: ({x,e}) => +x + 6*e,
+          h: ({y,e}) => +y + 3*e
+        },
+        {
+          w: ({z,e}) => +z + +e,
+          h: ({y,e}) => +y + 2*e
         }
       ],
       style: {
         alignItems: 'flex-end'
       }
+    },
+    {
+      name: 'C',
+      rectangles: [
+        {
+          w: ({y,e}) => (+y + 2*e) / 2,
+          h: ({z}) => z
+        },
+        {
+          w: ({x,e}) => +x + 2*e,
+          h: ({z,e}) => +z + 2*e
+        },
+        {
+          w: ({y,e}) => (+y + 2*e) / 2,
+          h: ({z}) => z
+        }
+      ]
+    },
+    {
+      name: 'D',
+      rectangles: [
+        {
+          w: ({z,e}) => +z + +e,
+          h: ({y,e}) => +y + 2*e
+        },
+        {
+          w: ({x,e}) => +x + 4*e,
+          h: ({y,e}) => +y + 2*e
+        },
+        {
+          w: ({z,e}) => +z + +e,
+          h: ({y,e}) => +y + 2*e
+        },
+      ]
+    },
+    {
+      name: 'E',
+      rectangles: [
+        {
+          w: ({y,e}) => (+y + 2*e) / 2,
+          h: ({z}) => z
+        },
+        {
+          w: ({x,e}) => +x + 2*e,
+          h: ({z,e}) => +z + +e
+        },
+        {
+          w: ({y,e}) => (+y + 2*e) / 2,
+          h: ({z}) => z
+        },
+      ]
     }
   ]
 }
